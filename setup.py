09d5eb4d4e0f24209297ec3fda6885d1a8b9add5
serdarildercaglar <87153193+serdarildercaglar@users.noreply.github.com>
2022-06-30 16:03:43 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='my_utils',
       version='0.1',
       description='...',
-      url='http://github.com/andrewm4894',
-      author='Maguire, Andrew',
-      author_email='...',
+      url='http://github.com/serdarildercaglar',
+      author='serdar caglar',
+      author_email='serdarildercaglar@gmail.com',
       license='GPLv2',
       packages=find_packages(),
       zip_safe=False)
